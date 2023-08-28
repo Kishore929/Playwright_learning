136c6f32e19c9248cedc8037180279eec7baa5ec
kishore meesala <kishoremeesala2@gmail.com>
2023-08-28 19:30:55 +0530

    checking

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,6 +3,7 @@ const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
+  // checking the changes
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
